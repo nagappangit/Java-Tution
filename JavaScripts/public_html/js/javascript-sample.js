f6d04b864b6db1a82b66a906fac6000be01c91e7
nagappan <nagarajeshbe@gmail.com>
2016-11-17 21:04:15 +0530

    Simple Java Scripts - Variables

--- JavaScripts/public_html/js/javascript-sample.js
+++ JavaScripts/public_html/js/javascript-sample.js
@@ -13,12 +13,27 @@ function myFunction() {
         return a - b;
     });
     document.getElementById("SNo").innerHTML = a;
-    
+
 }
 //if statement
 var name = "Nagappan";
 if (name = "Nagappan")
 {
-alert("Welcome to Java Script Tution");
+    alert("Welcome to Java Script Tuition");
 }
-
+else
+{
+    alert("Thank You");
+}
+function myWhile() {
+    var increment = 0;
+    var text;
+    text = "<p>";
+    while (increment < a.length) {
+        text += a[increment] + "<br />";
+        increment++;
+    }
+    text += "</p>";
+    document.getElementById("whilecon").innerHTML = text;
+    //document.getElementById("whilecon").innerHTML = increment;
+}
