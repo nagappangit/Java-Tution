796d46766239db1f705a3ef50e6e41f31a0b1ebe
nagappan <nagarajeshbe@gmail.com>
2016-11-17 23:11:49 +0530

    Simple Java Scripts - Variables

--- JavaScripts/public_html/js/eventhandler.js
+++ JavaScripts/public_html/js/eventhandler.js
@@ -3,68 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-var azteclink = {
-    init: function ()
-    {
-        var link = document.getElementByTagName("a");
-        link.onclick = azteclink.clickHandler;
-    },
-    clickHandler: function ()
-    {
-        alert("Web Design and Development Company in Chennai");
-    }
-};
-Core.start(azteclink);
-var Tooltips = {
-    init: function () {
-        var links = document.getElementById("aztec");
-        for (var i = 0; i < links.length; i++)
-        {
-            var title = links[i].title;
-            if (title && title.length > 0)
-            {
-                Core.addEventListener(links[i], "mouseover", Tooltips.showTipListener);
-                Core.addEventListener(links[i], "focus", Tooltips.showTipListener);
-                Core.addEventListener(links[i], "mouseout", Tooltips.hideTipListener);
-                Core.addEventListener(links[i], "mouseover", Tooltips.hideTipListener);
-
-            }
-        }
-
-    },
-    showTip: function (link)
-    {
-        Tooltips.hideTip(link);
-        var tip = document.createElement("span");
-        tip.className = "tooltip";
-        var tipText = document.createTextNode(link.title);
-        tip.appendChild(tipText);
-        link.appendChild(tip);
-        link._tooltip = tip;
-        link.title = "";
-// Fix for Safari2/Opera9 repaint issue
-        document.documentElement.style.position = "relative";
-    },
-    hideTip: function (link)
-    {
-        if (link._tooltip)
-        {
-            link.title = link._tooltip.childNodes[0].nodeValue;
-            link.removeChild(link._tooltip);
-            link._tooltip = null;
-// Fix for Safari2/Opera9 repaint issue
-            document.documentElement.style.position = "static";
-        }
-    },
-    showTipListener: function (event)
-    {
-        Tooltips.showTip(this);
-        Core.preventDefault(event);
-    },
-    hideTipListener: function (event)
-    {
-        Tooltips.hideTip(this);
-    }
-};
-Core.start(Tooltips);
+function myFunction() {
+    alert("Web Design and Development Company in Chennai");
+}
+//var Tooltips = {
+//    init: function () {
+//        var links = document.getElementById("aztec");
+//        for (var i = 0; i < links.length; i++)
+//        {
+//            var title = links[i].title;
+//            if (title && title.length > 0)
+//            {
+//                Core.addEventListener(links[i], "mouseover", Tooltips.showTipListener);
+//                Core.addEventListener(links[i], "focus", Tooltips.showTipListener);
+//                Core.addEventListener(links[i], "mouseout", Tooltips.hideTipListener);
+//                Core.addEventListener(links[i], "mouseover", Tooltips.hideTipListener);
+//
+//            }
+//        }
+//
+//    },
+//    showTip: function (link)
+//    {
+//        Tooltips.hideTip(link);
+//        var tip = document.createElement("span");
+//        tip.className = "tooltip";
+//        var tipText = document.createTextNode(link.title);
+//        tip.appendChild(tipText);
+//        link.appendChild(tip);
+//        link._tooltip = tip;
+//        link.title = "";
+//        document.documentElement.style.position = "relative";
+//    },
+//    hideTip: function (link)
+//    {
+//        if (link._tooltip)
+//        {
+//            link.title = link._tooltip.childNodes[0].nodeValue;
+//            link.removeChild(link._tooltip);
+//            link._tooltip = null;
+//            document.documentElement.style.position = "static";
+//        }
+//    },
+//    showTipListener: function (event)
+//    {
+//        Tooltips.showTip(this);
+//        Core.preventDefault(event);
+//    },
+//    hideTipListener: function (event)
+//    {
+//        Tooltips.hideTip(this);
+//    }
+//};
